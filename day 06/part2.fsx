b7dda9d03c4d666547a86ac58b0a7118f1cfca3a
Jo Van Eyck <jovaneyck1988@hotmail.com>
2025-12-06 15:45:52 +0100

    06.2 take 2

--- day 06/part2.fsx
+++ day 06/part2.fsx
@@ -11,36 +11,31 @@ let example =
   6 98  215 314
 *   +   *   +  """.Split("\n")
     |> List.ofSeq
-
-let splitindices =
-    let operators = input |> List.last
-    [ for i in 0 .. operators.Length - 1 do
-        if operators[i] <> ' ' then
-            yield i ]
-
-let rec split indices (input : char list list) =
-    match indices with
-    | [] -> [input]
-    | i :: is ->
-        let splits =
-            input
-            |> List.map (fun line -> line |> List.ofSeq |> List.splitAt i)
-        let problem = splits |> List.map fst
-        let rest = splits |> List.map snd
-        let nis = is |> List.map (fun ii -> ii - i)
-        problem :: (split nis rest)
-
-let rawProblems = split splitindices (input |> List.map List.ofSeq) |> List.skip 1        
-
-let solve problem =
-    let operator = problem |> List.last |> List.head
-    let operands = problem[0..(problem.Length - 2)] |> List.transpose |> List.map ((List.map string) >> (String.concat "") >> (fun nb -> nb.TrimEnd())) |> List.filter ((<>) "") |> List.map int64
+    
+let chunk delim_pred lst =
+    let rec chunk acc current = function
+        | [] -> (current :: acc) |> List.rev |> List.map List.rev
+        | x :: xs when delim_pred x -> chunk (current :: acc) [] xs
+        | x :: xs -> chunk acc (x :: current) xs
+    chunk [] [] lst
+    
+let parse (problem : char list list) =
+    let [operator], rest = problem[0] |> List.rev |> List.splitAt 1
+    let last_line_without_operator = rest |> List.rev
+    let numbers =
+        last_line_without_operator :: problem[1..]
+        |> List.map ((List.map string) >> (String.concat "") >> _.Trim() >> int64)
+    operator, numbers
+    
+let solve (operator, numbers) =
     match operator with
-    | '*' -> operands |> List.reduce (*)
-    | '+' -> operands |> List.reduce (+)
-    | c -> failwith $"Unexpected operator: {c}"
+    | '*' -> numbers |> List.reduce (*)
+    | '+' -> numbers |> List.reduce (+)
 
-let solutions = rawProblems |> List.map solve |> List.sum
+let transposed = example |> Seq.transpose |> Seq.map Seq.toList |> Seq.toList
+let problems = transposed |> chunk (List.forall ((=) ' '))
+let problem = problems[0]
+let solution = problems |> List.map (parse >> solve) |> List.sum
 
 let run () =
     printf "Testing.."
